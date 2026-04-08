ed6b1d438d00b8a98007ed611d9ba5d38ad3c414
Markus Schaub <markus.git@icloud.com>
2026-04-08 23:45:13 +0200

    extracted parsing from analyzer to parser.py

---
+++ parser.py
@@ -0,0 +1,73 @@
+import re
+from typing import TypedDict
+
+class ChatEntry(TypedDict):
+    Stream: str
+    Number: str
+    Time: str
+    Chatter: str
+    Message: str
+
+def parse_file(file_name: str, lines: list[str]) -> list[ChatEntry]:
+    file_chats: list[ChatEntry] = []
+    stream = file_name.removesuffix(".srt")
+
+    length = len(lines)
+    if length % 4 != 0:
+        raise ValueError(f"Can't parse file {file_name}, invalid number of lines")
+
+    for i in range(0, length, 4):
+        number = parse_number(lines[i].rstrip())
+        time = parse_time(lines[i + 1].rstrip())
+        chatter, message = parse_message(lines[i + 2].rstrip())
+        if number and time and chatter:
+            file_chats.append(
+                {
+                    "Stream": stream,
+                    "Number": number,
+                    "Time": time,
+                    "Chatter": chatter,
+                    "Message": message,
+                }
+            )
+        else:
+            if not number:
+                error_line = 1
+            elif not time:
+                error_line = 2
+            else:
+                error_line = 3
+
+            error_message = (
+                f"Can't parse file {file_name}, first error: line {i+error_line}"
+            )
+            raise ValueError(error_message)
+
+    return file_chats
+
+
+def parse_number(line: str) -> str:
+    pattern = r"^\d+$"
+    if match := re.search(pattern, line):
+        return match.group(0)
+    else:
+        return ""
+
+
+def parse_time(line: str) -> str:
+    pattern_time = r"(\d{2}(?::[0-5][0-9]){2}),\d{3}"
+    pattern = r"^" + pattern_time + r" --> " + pattern_time + r"$"
+    if match := re.search(pattern, line):
+        return match.group(1)
+    else:
+        return ""
+
+
+def parse_message(line: str) -> tuple[str, str]:
+    pattern = r"^@?(.+?): ?(.*)$"
+    if match := re.search(pattern, line):
+        chatter = match.group(1)
+        message = match.group(2)
+        return (chatter, message)
+    else:
+        return ("", "")
